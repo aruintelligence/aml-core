5c87383092b3cc1e09e80743aaeceb7c6a4f879b
Daniel Jacob Read IV, CEO <aruintelligence@gmail.com>
2026-09-07 20:07:20 -0700

    Expose AML language intelligence APIs

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ export { analyzeAMT } from "./compiler/diagnostics.js";
 export { explainCompilation } from "./compiler/explain.js";
 export { verifyBuildManifest } from "./compiler/verifyBuild.js";
 export { ethicalRenderGate } from "./runtime/ethicalRenderGate.js";
+export { getCompletionItems, getHoverInfo, getLanguageCatalog } from "./tooling/languageService.js";
