a777c1f543e2f80aec01562af31ac83e30ddb5d4
Daniel Jacob Read IV, CEO <aruintelligence@gmail.com>
2026-09-07 20:03:47 -0700

    Expose AML build verification API

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export { compileAML, compileSource } from "./compiler/compiler.js";
 export { analyzeAMT } from "./compiler/diagnostics.js";
+export { verifyBuildManifest } from "./compiler/verifyBuild.js";
 export { ethicalRenderGate } from "./runtime/ethicalRenderGate.js";
