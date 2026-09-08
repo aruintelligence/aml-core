f069a854defc65e11d815d2f2642c6a33d65f840
Daniel Jacob Read IV, CEO <aruintelligence@gmail.com>
2026-09-07 21:57:21 -0700

    Export AML brand authorization credentials

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ export { createThresholdAuthorization, verifyThresholdAuthorization } from "./ru
 export { signCapabilityToken, verifyCapabilityToken } from "./runtime/capabilityToken.js";
 export { createRevocationRegistry, revokeArtifact, verifyRevocationRegistry, isRevoked } from "./runtime/revocationRegistry.js";
 export { createProofCarryingInterface, verifyProofCarryingInterface } from "./runtime/proofCarryingInterface.js";
+export { signBrandAuthorization, verifyBrandAuthorization } from "./runtime/brandAuthorization.js";
 export { canonicalize, canonicalJSONStringify } from "./protocol/canonicalJson.js";
 export { createWireEnvelope, validateWireEnvelope, negotiateWireSession } from "./protocol/wireProtocol.js";
 export { createReplayGuard, acceptWireEnvelope } from "./protocol/replayGuard.js";
